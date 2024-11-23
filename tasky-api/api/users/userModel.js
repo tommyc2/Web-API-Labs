941d2c9ec081b4b519d082f4de4a41a763616da4
tommyc2 <20101841@mail.wit.ie>
2024-11-23 19:51:04 +0000

    Mongo lab Exercises done

--- tasky-api/api/users/userModel.js
+++ tasky-api/api/users/userModel.js
@@ -4,7 +4,17 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {
+    type: String, required: true
+  }
 });
+
+// Validator for password
+const passwordValidator = (pw) => {
+  // Referenced Material: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
+  const regex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/;
+  return regex.test(pw) // returns true if password matches regex, false otherwise
+}
+UserSchema.path("password").validate(passwordValidator);
 
 export default mongoose.model('User', UserSchema);
