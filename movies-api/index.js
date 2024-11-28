280baed3040419e3beaf44fc22cd70afb8c54361
tommyc2 <20101841@mail.wit.ie>
2024-11-28 11:56:23 +0000

    Initialise Dev Env - Data

---
+++ movies-api/index.js
@@ -0,0 +1,22 @@
+// Main entry point for application //
+
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import usersRouter from './api/users';
+import './db';
+import defaultErrHandler from './errHandler'
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT; 
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/users', usersRouter);
+app.use(defaultErrHandler);
+
+app.listen(port, () => {
+  console.info(`Server running at ${port}`);
+});
