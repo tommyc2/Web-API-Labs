5d1c16ade128e1e4b27d2e60eef526cd62eee8e7
tommyc2 <20101841@mail.wit.ie>
2024-11-28 15:08:07 +0000

    Authentication lab - exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -1,7 +1,7 @@
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
-import {getUpcomingMovies} from '../tmdb-api';
+import {getGenres, getUpcomingMovies} from '../tmdb-api';
 
 const router = express.Router();
 
@@ -40,6 +40,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+    const movieGenresList = await getGenres();
+    res.status(200).json(movieGenresList);
 }));
 
 export default router;
