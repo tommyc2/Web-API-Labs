a9889cd3f90ef1236edcdefe01c6dd6e317b489d
tommyc2 <20101841@mail.wit.ie>
2024-12-15 20:03:09 +0000

    Integration Lab - Integrate Users Endpoints

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -4,6 +4,27 @@ export const getMovies = async () => {
     const response = await  fetch(
       `https://api.themoviedb.org/3/discover/movie?api_key=${tmdb_api_key}&language=en-US&include_adult=false&page=1`
     )
-    console.log("Api key", tmdb_api_key)
     return response.json()
   };
+
+  export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
